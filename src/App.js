8905bdaaf4aaa9ca1a8f89b045977068dc635f54
baileyben <l3enl3ailey@hotmail.com>
2022-10-10 17:53:17 +1100

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+
 
 
 
@@ -12,16 +14,7 @@ function App() {
 
  return (
 <div >
-<AuthContextProvider>
-    <Router basename="/Crypto-Currency-Tracker-V2">
-        <Routes>
-<Navbar />
-<Hero />
-<About />
-<Footer />
-</Routes>
-</Router>
-    </AuthContextProvider>
+<Home/>
 </div>
  )
   
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import About from './About'
+import Footer from './Footer'
+import Hero from './Hero'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <div>
+<Navbar />
+<Hero />
+<About />
+<Footer />
+    </div>
+  )
+}
+
+export default Home
